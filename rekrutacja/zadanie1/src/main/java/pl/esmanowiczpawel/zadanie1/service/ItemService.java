2301d3f06e7e4a50997a72cf7735e82426b7c4a9
Paweł <30297982+esiu06@users.noreply.github.com>
2021-12-05 11:06:10 +0100

    Update ItemService.java

--- rekrutacja/zadanie1/src/main/java/pl/esmanowiczpawel/zadanie1/service/ItemService.java
+++ rekrutacja/zadanie1/src/main/java/pl/esmanowiczpawel/zadanie1/service/ItemService.java
@@ -1,6 +1,7 @@
 package pl.esmanowiczpawel.zadanie1.service;
 
 import org.springframework.stereotype.Service;
+import pl.esmanowiczpawel.zadanie1.dto.ItemDto;
 import pl.esmanowiczpawel.zadanie1.model.Item;
 import pl.esmanowiczpawel.zadanie1.repository.ItemRepository;
 
@@ -17,7 +18,11 @@ public class ItemService {
         return itemRepository.findAll();
     }
 
-    public Item save(Item item) {
+    public Item save(ItemDto itemDto) {
+        Item item = new Item();
+        item.setID_GUID(itemDto.getID_GUID());
+        item.setName(itemDto.getName());
+
         return itemRepository.save(item);
     }
 }
